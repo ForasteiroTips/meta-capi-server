a3da219309727fc2744107922e6d072855115da3
Forasteiro Tips <franciscogama2009@gmail.com>
2025-04-30 08:22:00 -0300

    Update server.js

--- server.js
+++ server.js
@@ -18,7 +18,10 @@ app.post('/send-event', async (req, res) => {
         event_id,
         action_source: "website",
         event_source_url: "https://forasteirotips.github.io/forasteiro/",
-        user_data
+        user_data: {
+          ...user_data,
+          external_id: event_id // ajuda o Meta a reconhecer como servidor
+        }
       }]
     };
 
